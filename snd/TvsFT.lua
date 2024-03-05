0c257498e43172892ebb0d91f46aa14912eb22d1
Eden <41390133+elijabesu@users.noreply.github.com>
2024-03-05 11:10:16 +0100

    Update TvsFT.lua

--- snd/TvsFT.lua
+++ snd/TvsFT.lua
@@ -1,13 +1,11 @@
-t = GetTargetHPP()
-f = GetFocusTargetHPP()
-diff = math.floor(math.abs(t-f))
-substr = "%!"
-str = "Focus %s, current difference is %d%s"
+channel = "/p" --change to /y for Bozja
+
+diff = math.floor(math.abs(GetTargetHPP() - GetFocusTargetHPP()))
 if (t > f and diff > 3) then
-   toSend = string.format(str, GetTargetName(), diff, substr)
+   toSend = "Focus "..GetTargetName()..", current difference is "..diff.."%!"
 elseif (f > t and diff > 3) then
-   toSend = string.format(str, GetFocusTargetName(), diff, substr)
+   toSend = "Focus "..GetFocusTargetName()..", current difference is "..diff.."%!"
 else
    toSend = "Targets are even!"
 end
-yield("/echo "..toSend)
+yield(channel.." "..toSend)
