afb5f33a0a2e763e573c33b711e03d6a04f7f0ca
Eden <41390133+elijabesu@users.noreply.github.com>
2024-03-05 11:10:14 +0100

    Update SwiftCD.lua

--- snd/SwiftCD.lua
+++ snd/SwiftCD.lua
@@ -3,5 +3,5 @@ swift = math.floor(GetSpellCooldown(7561))
 if swift == 0 then
    yield("/p Swiftcast ready!")
 else
-   yield(string.format("/p Swiftcast ready in %d seconds!", swift))
+   yield("/p Swiftcast ready in "..swift.." seconds!")
 end
