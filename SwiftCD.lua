779cae70c9ad713c62078edaa668f352b5ac00fe
Eden <41390133+elijabesu@users.noreply.github.com>
2025-11-26 22:34:18 +0100

    added metadata

--- SwiftCD.lua
+++ SwiftCD.lua
@@ -1,7 +1,19 @@
-swift = math.floor(Actions.GetActionInfo(7561).RealSpellCooldown)
+--[=====[
+[[SND Metadata]]
+author: elijabesu
+version: 2.0.0
+description: Just a simple swiftcast macro without the annoying --:--.
 
-if swift == 0 then
-	yield("/p Swiftcast ready!")
-else
-	yield("/p Swiftcast ready in "..swift.." seconds!")
+[[End Metadata]]
+--]=====]
+
+--[[
+********************************************************************************
+*            Code: Don't touch this unless you know what you're doing          *
+********************************************************************************
+--]]
+local swift = math.floor(Actions.GetActionInfo(7561).RealSpellCooldown)
+
+if swift == 0 then yield("/p Swiftcast ready!")
+else yield("/p Swiftcast ready in "..swift.." seconds!")
 end
